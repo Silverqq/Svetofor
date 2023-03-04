279c5ec98d79c21ba8533759d03f4cc76bcedb07
Silver <122754864+Silverqq@users.noreply.github.com>
2023-03-04 15:59:04 +0300

    исправил ошибку с вылетом

--- MainActivity.java
+++ MainActivity.java
@@ -1,35 +1,33 @@
 package com.example.svetofor;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.view.View;
 import android.graphics.Color;
-import android.widget.AdapterView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private LinearLayout mBackgroundLinearLayout;
-
-    private String[] colors = { "BLUE", "CYAN", "DKGRAY",
-            "GRAY", "GREEN", "LTGRAY", "MAGENTA", "RED", "TRANSPARENT",
-            "WHITE", "YELLOW" };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBackgroundLinearLayout = findViewById(R.id.layout123);
     }
-    public void OnClicksetredlayout(View view){
-        mBackgroundLinearLayout.setBackgroundColor(Integer.parseInt("#FF0000"));
+
+    public void OnClicksetredlayout(View view) {
+        LinearLayout backlayout = findViewById(R.id.layout123);
+        backlayout.setBackgroundColor(Color.RED);
     }
-    public void OnClicksetyellowlayout(View view){
-        mBackgroundLinearLayout.setBackgroundColor(Integer.parseInt("#FFE500"));
+
+    public void OnClicksetyellowlayout(View view) {
+        LinearLayout backlayout = findViewById(R.id.layout123);
+        backlayout.setBackgroundColor(Color.YELLOW);
     }
-    public void OnClicksetgreenlayout(View view){
-        mBackgroundLinearLayout.setBackgroundColor(Integer.parseInt("#00FF0A"));
+
+    public void OnClicksetgreenlayout(View view) {
+        LinearLayout backlayout = findViewById(R.id.layout123);
+        backlayout.setBackgroundColor(Color.GREEN);
     }
 }
